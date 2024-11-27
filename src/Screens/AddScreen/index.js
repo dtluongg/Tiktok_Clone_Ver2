fa49e8d3f89ced3082b457ca8ff2d4d3b75c239d
dtluongg <fastdominicracing@gmail.com>
2024-11-27 14:40:49 +0700

    unable .gitgnore .env.local

--- src/Screens/AddScreen/index.js
+++ src/Screens/AddScreen/index.js
@@ -1,30 +1,39 @@
-import React, { useEffect } from 'react';
-import {View, Text} from 'react-native';
-import {useUser} from "@clerk/clerk-expo";
-import {supabase} from "../../Hooks/supabaseConfig"
-
+import React, { useEffect } from "react";
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import { useUser } from "@clerk/clerk-expo";
+import { supabase } from "../../Hooks/supabaseConfig";
+import * as ImagePicker from "expo-image-picker";
 //import create:
-import styles from './style'
+import styles from "./style";
 
 const AddScreen = () => {
-    const {user} = useUser();
-    useEffect(()=>{
-        user&&updateProfileImage();
-    },[user])
-    const updateProfileImage =async () => {
-        const {data, error} = await supabase
-        .from('Users')
-        .update({'profileImage': user?.imageUrl})
-        .eq('email',user?.primaryEmailAddress?.emailAddress)
-        .is('profileImage', null)
-        .select();
-        console.log(data)
-    }
-    return(
-        <View style = {styles.homeView}>
-            <Text> This is Add Screen</Text>
+    const handleSelectVideoFile = async () => {
+        // No permissions request is necessary for launching the image library
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ["images", "videos"],
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1,
+        });
+
+        console.log(result);
+
+        if (!result.canceled) {
+            console.log(result.assets[0].uri);
+            setImage(result.assets[0].uri);
+        }
+    };
+
+    return (
+        <View style={styles.AddView}>
+            <Image style={styles.imageIcon} source={require("../../img/AddScreen/upload.png")} />
+            <Text style={styles.title}>Start Uploading Short Video</Text>
+            <Text style={styles.titleSub}>Lets upload short your video</Text>
+            <TouchableOpacity style={styles.buttonUpload} onPress={handleSelectVideoFile}>
+                <Text style={styles.titleSubmit}>Select Your Video</Text>
+            </TouchableOpacity>
         </View>
-    )
-}
+    );
+};
 
 export default AddScreen;
--- src/Screens/AddScreen/style.js
+++ src/Screens/AddScreen/style.js
@@ -1,11 +1,39 @@
 import { StyleSheet } from "react-native";
-
+import color from "../../styles";
 const styles = StyleSheet.create({
-    homeView:{
+    AddView:{
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-    }
+    },
+    imageIcon:{
+        width: 140,
+        height: 140,
+    },
+    title: {
+        fontFamily: "outfit-bold", // font lấy từ string font đc khai báo trong useFonts
+        color: "#000",
+        fontSize: 25,
+        marginTop: 20,
+    },
+    titleSub: {
+        fontFamily: "outfit-medium",
+        color: "#000",
+        fontSize: 15,
+        marginTop: 20,
+    },
+    buttonUpload:{
+        backgroundColor: color.BLACK,
+        padding: 10,
+        paddingHorizontal: 25,
+        borderRadius: 99,
+        marginTop: 20,
+    },
+    titleSubmit: {
+        fontFamily: "outfit-regular",
+        color: "#fff",
+        fontSize: 15
+    },
 });
 
 export default styles;
