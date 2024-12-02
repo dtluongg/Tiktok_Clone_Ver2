88fcd9c553c2ec2bf116445264fb6d1794621df6
dtluongg <fastdominicracing@gmail.com>
2024-12-02 16:57:35 +0700

    Done Push Video To AWS

--- src/Hooks/S3Bucket.js
+++ src/Hooks/S3Bucket.js
@@ -1,7 +1,7 @@
 import {S3} from "aws-sdk";
 
 export const s3bucket = new S3({
-    accessKeyId: '',
-    secretAccessKey:'',
-    region: '',
+    accessKeyId: 'AKIA5CBGTQAHL7F7ZOC7',
+    secretAccessKey: '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',
+    region: process.env.EXPO_PUBLISH_AWS_REGION,
 }) 
--- src/Screens/AddScreen/Add_Screen_Before.js
+++ src/Screens/AddScreen/Add_Screen_Before.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import {
     View,
     Text,
@@ -11,28 +11,75 @@ import {
 import { useRoute } from "@react-navigation/native";
 import styles from "./style";
 import Ionicons from "@expo/vector-icons/Ionicons";
+import { s3bucket } from "../../Hooks/S3Bucket";
+import { supabase } from "../../Hooks/supabaseConfig";
 
-const Before_Add_View = ({navigation}) => {
+const Before_Add_View = ({ navigation }) => {
     const createParams = useRoute().params;
-    
-
+    const [description, setDescription] = useState();
+    const [videoUrl, setVideoUrl] = useState();
     useEffect(() => {
         console.log(createParams);
     }, []);
+
+    const UploadFileToAws = async (file, type) => {
+        const fileType = file.split(".").pop();
+        const params = {
+            Bucket: "tiktok-app",
+            Key: `videotiktok-${Date.now()}.${fileType}`, // Ví dụ: videotiktok-23134234.mp4
+            Body: await fetch(file).then((resp) => resp.blob()),
+            ACL: "public-read",
+            ContentType: type == "video" ? `video/${fileType}` : `image/${fileType}`,
+        };
+        try {
+            const resp = await s3bucket.upload(params).promise();
+            console.log("File Upload Success:", resp?.Location);
+            return resp?.Location;
+        } catch (e) {
+            console.log("File Upload Error:", e);
+            return null;
+        }
+    };
+    const handlePublish = async () => {
+        const videoUrl = await UploadFileToAws(createParams.video, "video");
+        const thumbnailUrl = await UploadFileToAws(createParams.thumbnail, "image");
+        const { data: session } = await supabase.auth.getSession();
+        const email = session?.user?.email || "fastdominicracing@gmail.com";
+        const postData = {
+            videoUrl: videoUrl,
+            thumbnail: thumbnailUrl,
+            description: description,
+            emailRef: email,
+        };
+
+        const { dataPost, error } = await supabase.from("PostList").insert([postData]).select();
+
+        if (error) {
+            console.log("Supabase insert error: ", error);
+        } else {
+            console.log("Post Data inserted", dataPost);
+        }
+    };
     return (
         <KeyboardAvoidingView style={styles.AddViewBefore}>
             <ScrollView style={styles.viewScroll}>
                 <TouchableOpacity
-                    onPress={()=>navigation.goBack()}
-                    style={{flexDirection: 'row', alignItems: 'center', marginTop: 20}}
+                    onPress={() => navigation.goBack()}
+                    style={{ flexDirection: "row", alignItems: "center", marginTop: 20 }}
                 >
-                    <Ionicons name="arrow-back-circle" size={44} color="black" style={{ marginRight: 10 }}/>
-                    <Text style={{fontSize: 20}}>Go Back</Text>
+                    <Ionicons
+                        name="arrow-back-circle"
+                        size={44}
+                        color="black"
+                        style={{ marginRight: 10 }}
+                    />
+                    <Text style={{ fontSize: 20 }}>Go Back</Text>
                 </TouchableOpacity>
                 <View style={styles.viewBeforeContainer}>
                     <Text style={styles.title}>This is Before Add View</Text>
                     <Image source={{ uri: createParams?.thumbnail }} style={styles.image} />
                     <TextInput
+                        onChangeText={(value) => setDescription(value)}
                         numberOfLines={3}
                         placeholder="Description"
                         style={{
@@ -44,7 +91,7 @@ const Before_Add_View = ({navigation}) => {
                             paddingHorizontal: 20,
                         }}
                     />
-                    <TouchableOpacity style={styles.buttonUpload}>
+                    <TouchableOpacity style={styles.buttonUpload} onPress={handlePublish}>
                         <Text style={styles.titleSubmit}>Publish Video</Text>
                     </TouchableOpacity>
                 </View>
